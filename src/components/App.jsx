9dae82d31fd3183703209203c6b63ea73c0c0f7e
xrsQugi <02.04.5.art@gmail.com>
2023-07-16 18:08:41 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,53 +5,6 @@ import Section from "./Section/Section";
 import Notification from './Notification/Notification';
 import css from './App.module.css';
 
-// export default function App () {
-//   const [good, setGood] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [bad, setBad] = useState(0);
-
-//   const feedbackOptionsArr = [
-//     {
-//       id: 1,
-//       name: 'good',
-//       action: () => setGood(good => good + 1),
-//     },
-//     {
-//       id: 2,
-//       name: 'neutral',
-//       action: () => setNeutral(neutral => neutral + 1),
-//     },
-//     {
-//       id: 3,
-//       name: 'bad',
-//       action: () => setBad(bad => bad + 1),
-//     },
-//   ];
-
-//   const countTotalFeedback = () => {
-//     return good + neutral + bad;
-//   }
-
-//   const countPositiveFeedbackPercentage = () => {
-//     return (Math.round(good / countTotalFeedback() * 100));
-//   }
-
-//   return (
-//           <div className={css.feedback}>
-//             <Section title="Please leave feedback">
-//               <FeedbackOptions options={feedbackOptionsArr}/>
-//             </Section>
-//             <Section title="Statistic">
-//               {countTotalFeedback() ? (
-//                 <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}/>
-//                 ) : (
-//                 <Notification notify='There is no feedback'/>
-//               )}
-//             </Section>
-//           </div>
-//         )
-// }
-
 export default function App() {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
